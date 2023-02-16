f02df8d64d4eac63a5573243340923d8a08a5add
Acckerman <109450912+Acckerman@users.noreply.github.com>
2023-02-16 22:04:05 +0530

    Update config.py
    Signed-off-by: Acckerman <109450912+Acckerman@users.noreply.github.com>

--- config.py
+++ config.py
@@ -12,12 +12,12 @@ FORCE_SUB = os.environ.get("FORCE_SUB", "k2m_movies_series")
 
 DB_NAME = os.environ.get("DB_NAME","Autofilterpro")     
 
-DB_URL = os.environ.get("DB_URL","mongodb+srv://Autofilterpro: acckerman@cluster0.apoqe80.mongodb.net/?retryWrites=true&w=majority")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://Autofilterpro:acckerman@cluster0.apoqe80.mongodb.net/?retryWrites=true&w=majority")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
 START_PIC = os.environ.get("START_PIC", "https://graph.org/file/1f04a86d2526b25b34d55.jpg")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1753576629').split()]
 
 PORT = os.environ.get("PORT", "8080")
